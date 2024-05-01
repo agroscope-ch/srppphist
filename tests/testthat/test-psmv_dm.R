3dc8547f9b37bd820243d1ae72ad25ad32604a43
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2024-05-01 14:01:44 +0200

    Catch stray message in test with sink()
    The message is generated by the library called in download.file and
    is not caught by testthat::expect_message(). Therefore a workaround
    involving sink(type = "message") was used as described in
    https://stackoverflow.com/a/66139071

--- tests/testthat/test-psmv_dm.R
+++ tests/testthat/test-psmv_dm.R
@@ -2,12 +2,18 @@ test_that("Methods for reading in XML files work", {
 
   # The remaining message "trying URL '2024-01-03'" is from the system library
   # and is not detected by expect_message().
+  # Interception of this message by sink() was taken from https://stackoverflow.com/a/66139071
   expect_warning(
     expect_warning(
-      expect_error(
-        psmv_test <- psmv_dm("2024-01-03")),
-      "cannot open URL"),
-    "length 0")
+      expect_error({
+        nullcon <- file(nullfile(), open = "wb")
+        sink(nullcon, type = "message")
+        psmv_test <- psmv_dm("2024-01-03")
+        sink(type = "message")
+        close(nullcon)
+      }),
+    "cannot open URL"),
+  "length 0")
 
   expect_message(
     expect_message(psmv_test <- psmv_dm(as.Date("2024-01-03")), "Reading XML for 2024-01-03"),
