cbfb289a18038de68fa10962a28f770f5e125d77
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2025-01-08 15:09:16 +0100

    Improve auxiliary functions

--- R/composition.R
+++ R/composition.R
@@ -1,7 +1,57 @@
 #' Retrieve one or more product compositions
 #'
 #' @param pNbrs A numeric vector of one or more P-Numbers
+#' @param lang The language for the returned table
 #' @export
-composition <- function(pNbrs) {
-  filter(srppphist::srppp_compositions, pNbr %in% pNbrs)
+#' @examples
+#' pNbr("Karate", select = FALSE) |>
+#'   composition()
+composition <- function(pNbrs, lang = c("de", "fr", "it")) {
+  lang <- match.arg(lang)
+  filter(srppphist::srppp_compositions, pNbr %in% pNbrs) |>
+    left_join(srppphist::srppp_products, by = "pNbr", relationship = "many-to-many") |>
+    filter(!grepl("-", wNbr)) |>
+    left_join(srppphist::srppp_substances, by = "pk") |>
+    select(pNbr, name,
+      pk, matches(paste0("substance_", lang)),
+      percent, g_per_L,
+      matches(paste0("ingredient_", lang))) |>
+    arrange(pNbr, pk)
+}
+
+#' Retrieve and select a P-Number for a product name
+#'
+#' @param string The search string
+#' @export
+#' @examples
+#' pNbr("Karate")
+pNbr <- function(string, select = interactive()) {
+  # Get matching products
+  products <- srppphist::srppp_products |>
+    filter(grepl(string, name, ignore.case = TRUE))
+
+  if (nrow(products) == 0) {
+    stop("No products found")
+    return(NULL)
+  }
+
+  # If requested, have the user select a matching product
+  if (select) {
+    if (interactive()) {
+      if (nrow(products) > 1) {
+        products <- products |>
+          dplyr::mutate(row = row_number()) |>
+          select(row, everything())
+        print(products)
+        selected <- as.numeric(readline("Select a row: "))
+        products <- products |>
+          dplyr::filter(row == selected)
+      }
+    } else { # If not interactive or not requested, select the first match
+      products <- products |>
+        slice(1)
+    }
+  }
+
+  return(products[["pNbr"]])
 }
