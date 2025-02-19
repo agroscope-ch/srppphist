9518ad6112029b94daa2afafe41ab62272cdc336
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2025-02-19 22:04:44 +0100

    Adapt tests to current srppp
    In particular, resolving cultures to the leaf cultures for alternative
    product searches does not work with languages other than German.

--- tests/testthat/test-alternative_products.R
+++ tests/testthat/test-alternative_products.R
@@ -5,15 +5,19 @@ test_that("Alternative products are found", {
   skip_if(srppphist_idir_unavailable)
 
   expect_message(
-    {srppp_test <- srppp_dm(2024)}, 
+    {srppp_test <- srppp_dm(2024)},
     "Reading XML for 2024-01-03")
 
   lambda_delta_gaps <- alternative_products(srppp_test, c("Lambda-Cyhalothrin", "Deltamethrin"),
-    missing = TRUE)
+    missing = TRUE, resolve_cultures = FALSE)
   expect_equal(nrow(lambda_delta_gaps), 108L)
 
-  lambda_delta_gaps_it <- alternative_products(srppp_test, c("Lambda-Cialotrina", "Deltametrina"), 
-    missing = TRUE, lang = "it")
+  lambda_delta_gaps_resolved <- alternative_products(srppp_test, c("Lambda-Cyhalothrin", "Deltamethrin"),
+    missing = TRUE, resolve_cultures = TRUE)
+  expect_equal(nrow(lambda_delta_gaps_resolved), 112L)
+
+  lambda_delta_gaps_it <- alternative_products(srppp_test, c("Lambda-Cialotrina", "Deltametrina"),
+    missing = TRUE, lang = "it", resolve_cultures = FALSE)
   expect_equal(nrow(lambda_delta_gaps_it), 108L)
 
   expect_error(alternative_products(srppp_test, c("test"), lang = "at"))
